1dcb1c7871109e1e9bdf4a240fa6cecaa07159b8
Bartolomeo <meo.aimo@gmail.com>
2023-12-20 18:37:41 +0100

    solution 20.12.2023 lb

--- assignments/a2_log_barrier/solution.py
+++ assignments/a2_log_barrier/solution.py
@@ -69,8 +69,125 @@ def solve(nlp: NLP, Dout={}):
     x = nlp.getInitializationSample()
 
     #
-    # Write your code Here
-    #
+    ## create empty containers
+    Dout["xs"] = []
+    Dout["x0"] = np.copy(x)
+    Dout["xs"].append(np.copy(x))
+    Dout["f"] = []
+    Dout["g"] = []
+    Dout["B"] = []
+    Dout["mu"] = []
 
+    ## PARAMETERS
+    mu = 1
+    rho_mu_min = 0.5
+    rho_ls = 0.01
+    rho_a_plus = 1.2
+    rho_a_min = 0.5
+    rho_lb_plus = 1.2
+    rho_lb_min = 0.5
+    tol = 0.0001
+    i = 0
+    x_p = [np.inf]*len(x)
+    types = nlp.getFeatureTypes()
+    id_f = [i for i, t in enumerate(types) if t == OT.f]
+    id_ineq = [i for i, t in enumerate(types) if t == OT.ineq]
+    iinf= 10000000
+    chk = True
+
+    ## first Hessian evaluation
+    H = nlp.getFHessian(x)
+    y, J = nlp.evaluate(x)
+
+    while(np.linalg.norm(x-x_p)>= tol):
+        ## Parameters
+        k = 1  # alpha
+        lb = 1  # lambda
+        x_p = x
+
+        ## evaluate function
+        y, J = nlp.evaluate(x)
+        B = y[id_f[0]] - mu*np.sum(np.log(-y[1:]))
+        #tmp = [np.outer(J[k].T, J[k]) for k in id_ineq]
+        #Hesse = H + mu * np.sum([tmp[j-1] / (y[j] ** 2) for j in id_ineq], axis=0)
+        Hesse= H + mu*np.sum(1/y[1:,None,None]**2*np.dot(J[1:].T,J[1:]),axis = 0)
+        Jakob = J[id_f[0]]-mu*np.sum([J[j]/y[j] for j in id_ineq],axis=0)
+        delta = np.linalg.solve((Hesse + lb * np.identity(len(Hesse))), -Jakob)
+
+        ## pass values to containers
+        Dout["f"].append(np.copy(y[id_f[0]]))
+        Dout["g"].append(np.copy(y[id_ineq]))
+        Dout["B"].append(np.copy(B))
+        Dout["mu"].append(np.copy(mu))
+
+        print("mu: ", mu)
+        i += 1
+
+        ## Newton Method
+        while (np.linalg.norm(k * delta, np.inf) >= tol):
+            if Jakob.T @ delta > 0:     ## check for pos. definiteness
+                # delta = -J[0]
+                lb = -np.min(np.linalg.eigvals(Hesse)) + 0.001
+                delta = np.linalg.solve((Hesse + lb * np.identity(len(Hesse))), -Jakob)
+
+            ## evaluate function for Wolf conditions
+            x_plus = k * delta + x
+            y_new, J_new = nlp.evaluate(x_plus)
+            if y_new[id_ineq].all() < 0 or chk is True:
+                B_new = y_new[id_f[0]] - mu * np.sum(np.log(-y_new[1:]))
+            else:
+                B_new = iinf
+            ## new conditions
+            cond = B + rho_ls * np.dot(Jakob, k * delta)
+
+            ## backtracking line search
+            while B_new > (cond):
+                ## compute new parameters
+                k = rho_a_min * k  # minimize k (alpha)
+                lb = rho_lb_plus * lb  # maximize lambda
+
+                # compute new Wolf conditions and evaluate function
+                delta = np.linalg.solve((Hesse + lb * np.identity(len(Hesse))), -Jakob)  # recompute delta
+                x_plus = x + k * delta
+                y_new, J_new = nlp.evaluate(x_plus)
+                cond = B + rho_ls * np.dot(Jakob, k * delta)
+
+                if y_new[id_ineq].all() < 0 or chk is True:
+                    B_new = y_new[id_f[0]] - mu * np.sum(np.log(-y_new[1:]))
+                else:
+                    B_new = iinf
+                    #print("ff")
+
+                i += 1
+
+            ## reassign variable values
+            B = B_new
+            J = J_new
+            x = x_plus
+            ## evaluate function
+            Jakob = J[id_f[0]] - mu * np.sum([J[j] / y[j] for j in id_ineq], axis=0)
+            H = nlp.getFHessian(x)
+            #tmp = [np.outer(J[k].T,J[k]) for k in id_ineq]
+            #Hesse = H + mu * np.sum([tmp[j-1] / (y[j] ** 2) for j in id_ineq], axis=0)
+            Hesse = H + mu * np.sum(1 / y[1:, None, None] ** 2 * np.dot(J[1:].T, J[1:]), axis=0)
+            i += 1
+
+            ## pass values to containers
+            Dout["xs"].append(np.copy(x))
+            Dout["f"].append(np.copy(y[id_f[0]]))
+            Dout["g"].append(np.copy(y[id_ineq]))
+            Dout["B"].append(np.copy(B))
+            Dout["mu"].append(np.copy(mu))
+
+            ## compute k (alpha) and lb (lambda) value
+            k = min(rho_a_plus * k, 1)
+            lb = rho_lb_min * lb
+
+        ## compute new parameter mu
+        mu = rho_mu_min*mu
+        i += 1
+    print("i_log: ",i)
+    #print("g(x_out): ", y[id_ineq])
+    #
 
     return x
